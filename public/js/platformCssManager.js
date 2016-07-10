8a95089a18704da167f7f7e4ba34556f12f6cd3c
Rudolf Cicko <rudolfcicko2016@gmail.com>
2016-07-10 19:28:13 +0100

    Added javascript manager for platform for css

---
+++ public/js/platformCssManager.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+   if( navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPad/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+   ){
+     $('.leaderboard').css("width", "80%");
+    }
+   else {
+     
+    }
+});
